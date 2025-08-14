7a55d3f124423d6853ebacc5c596ae13cc4e5467
AilynMza <ailynmorales501@gmail.com>
2025-08-14 14:55:13 -0600

    ✨ ➕ Develop: add react-router-dom and set up routing in App.js - By EM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
-    <Home />
-    <Footer />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
