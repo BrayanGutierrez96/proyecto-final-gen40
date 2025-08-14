0472acac847014492b957da7904738eec07d13d3
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-08-13 22:32:41 -0500

    Desarrollo: Se agrega la primera parte del archivo referencias. BG

--- src/components/Referencias/Referencias.jsx
+++ src/components/Referencias/Referencias.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import imagenReferencias from "../../assets/imagenReferencias.png";
 
 function Referencias() {
   return (
-    <div>Referencias</div>
-  )
+    <section
+      className={`flex bg-[url(./imagenReferencias.png)] bg-no-repeat bg-center bg-cover flex-col items-center justify-center p-4 rounded-lg w-[80%]`}
+    >
+      <article className="flex flex-col items-center justify-center text-white text-2xl cursor-pointer hover:text-red-200">
+        <h2>REFER A FRIENDS</h2>
+        <p>And get $30!</p>
+      </article>
+      <article>
+        <button>Refer Here</button>
+      </article>
+    </section>
+  );
 }
 
-export default Referencias
+export default Referencias;
