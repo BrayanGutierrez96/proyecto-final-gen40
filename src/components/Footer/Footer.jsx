61deba3fdc72b6132a588ef79ba813b1bd95c12d
AilynMza <ailynmorales501@gmail.com>
2025-08-14 12:59:37 -0600

    🏗️ Develop: scaffold Footer component - By EM

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,80 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer  className="bg-gradient-to-b from-[#1A1E26] to-[#01100B] w-full">
+        <section >
+            <div>
+                <img src="" alt="" />
+                <p></p>
+            </div>
+
+            <div>
+                <div>
+                    <h3></h3>
+                    <div>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
+
+                <div>
+                    <h3></h3>
+                    <div>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <p></p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
     </footer>
   )
 }
