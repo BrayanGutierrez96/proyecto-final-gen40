7b8cad580c3ca7d910a4cb39194c6f1a10d80d35
AilynMza <ailynmorales501@gmail.com>
2025-08-14 12:14:18 -0600

    ✨  Develop: Import Footer component in App.jsx - By EM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
+    <>
     <Home />
-    
+    <Footer />
+    </>
   )
 }
 
