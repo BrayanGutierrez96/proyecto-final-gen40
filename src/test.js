f1c760097fa96f9873d669d41def0bbe10324171
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-08-18 20:56:02 -0500

    Pruebas: creo el workflow de test. BG

---
+++ src/test.js
@@ -0,0 +1,5 @@
+console.log("Hola desde el test")
+
+console.log("Prueba 1: Mostrando el componete x")
+console.log("Prueba 2: Mostrando el componete y")
+console.log("Prueba 3: Mostrando el componete z")
