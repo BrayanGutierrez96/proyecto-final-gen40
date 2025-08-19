378921f489a7921c146563c6f5ababbee0fa0f81
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-08-18 19:50:25 -0500

    Desarrollo: Se inica la creacion del componente de referidos

--- src/components/Referencias/Referencias.jsx
+++ src/components/Referencias/Referencias.jsx
@@ -3,7 +3,7 @@ import imagenReferencias from "../../assets/imagenReferencias.png";
 function Referencias() {
   return (
     <section
-      className={`flex bg-[url(./imagenReferencias.png)] bg-no-repeat bg-center bg-cover flex-col items-center justify-center p-4 rounded-lg w-[80%]`}
+      className={`flex bg-[url(./imagenReferencias.png)] bg-no-repeat bg-center bg-cover flex-col items-center justify-center p-4 rounded-lg w-[80%] py-30`}
     >
       <article className="flex flex-col items-center justify-center text-white text-2xl cursor-pointer hover:text-red-200">
         <h2>REFER A FRIENDS</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Referencias from '../components/Referencias/Referencias'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='flex w-full h-screen justify-center items-center'>
+        <Referencias/>
+    </div>
   )
 }
 
