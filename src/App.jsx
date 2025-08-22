f501f08400f27eb86a0e01bde2b1e338dc188228
Danielsasa <danielsasa1@hotmail.com>
2025-08-21 19:11:02 -0600

    S agrego navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
     <BrowserRouter>
+      <Navbar />
     <Routes>
       <Route path='/' element={<Home />}/>
     </Routes>
