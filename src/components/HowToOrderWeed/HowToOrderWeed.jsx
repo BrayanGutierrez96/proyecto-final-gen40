6f1578b9c79c38b916d968512c5313d302f03677
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-09-03 19:46:23 -0500

    Desarrollo: Se aplican unos pequeños cambios al componente Eductaion. BG

--- src/components/HowToOrderWeed/HowToOrderWeed.jsx
+++ src/components/HowToOrderWeed/HowToOrderWeed.jsx
@@ -7,7 +7,7 @@ function HowToOrderWeed() {
 
   return (
     <section
-      className="bg-[#01100B] relative pb-50 flex flex-col gap-20 lg:gap-30"
+      className="bg-[#01100B] relative pb-50 flex flex-col gap-20 lg:gap-30 mt-50"
       aria-labelledby="how-to-order-heading"
       role="main"
     >
